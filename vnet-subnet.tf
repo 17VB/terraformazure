d0e0349eb331a817cde2e20dc0275717e6420675
vilas <vbhoge5@gmail.com>
2023-01-07 18:17:05 +1000

    upload vnetsubnet

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -13,7 +13,7 @@ resource "azurerm_subnet" "psubnet" {
     virtual_network_name = azurerm_virtual_network.pvnet.name
     resource_group_name = azurerm_resource_group.prg.name
     address_prefixes = ["10.0.2.0/24"]
-    tags = var.tags
+    
 
   
 }
